81c4211e67150e4e27f4f603010e418075c6c512
Davide Calarco <s280174@studenti.polito.it>
2021-03-31 11:54:00 +0200

    Create project
    The initial db file is on the project root folder.

---
+++ src/main/kotlin/it/polito/group19/lab2/Lab2Application.kt
@@ -0,0 +1,11 @@
+package it.polito.group19.lab2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Lab2Application
+
+fun main(args: Array<String>) {
+    runApplication<Lab2Application>(*args)
+}
