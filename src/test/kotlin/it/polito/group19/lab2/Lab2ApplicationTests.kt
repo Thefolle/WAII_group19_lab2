e89e261f5e6fc6fe93a91b0ec437c3a8c8b35046
Davide Calarco <s280174@studenti.polito.it>
2021-04-15 10:15:31 +0200

    Start to implement integration tests, not finish
    Application runs successfully

--- src/test/kotlin/it/polito/group19/lab2/Lab2ApplicationTests.kt
+++ src/test/kotlin/it/polito/group19/lab2/Lab2ApplicationTests.kt
@@ -1,13 +1,22 @@
 package it.polito.group19.lab2
 
+import it.polito.group19.lab2.dto.WalletDto
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
 
 @SpringBootTest
-class Lab2ApplicationTests {
+abstract class Lab2ApplicationTests {
+
+//    @Autowired
+//    lateinit var testRestTemplate: TestRestTemplate
 
     @Test
     fun contextLoads() {
+//        println("Test result: " + testRestTemplate.postForObject("http://localhost:8080/wallets", 1, WalletDto::class.java).wid)
+//        assertTrue(false)
     }
 
 }
