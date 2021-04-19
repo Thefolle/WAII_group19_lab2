471ee86bc9303ab925b6729258a990064d07e1d3
Davide Calarco <s280174@studenti.polito.it>
2021-04-19 17:14:47 +0200

    Configure successfully for repository testing
    Unit and integration tests require each a separate
    configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,4 +45,7 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
 }
--- src/test/kotlin/it/polito/group19/lab2/Lab2ApplicationTests.kt
+++ src/test/kotlin/it/polito/group19/lab2/Lab2ApplicationTests.kt
@@ -1,18 +1,27 @@
 package it.polito.group19.lab2
 
 import it.polito.group19.lab2.dto.WalletDto
+import it.polito.group19.lab2.repositories.UserRepository
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.test.context.TestExecutionListeners
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener
 
-@SpringBootTest
-abstract class Lab2ApplicationTests {
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class Lab2ApplicationTests {
+
 
     @Test
-    fun contextLoads() {
-          assertTrue(false)
+    fun `Here my test`() {
+
+          assertTrue(true)
     }
 
 }
